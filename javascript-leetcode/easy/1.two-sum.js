9e0f2850d14759eaabf401787aee107dcea6f30e
Andrew Lovato <andrewlovato@Andrews-MacBook-Pro.local>
2023-06-22 10:38:03 -0600

    code problem finished for palindrome problem

--- javascript-leetcode/easy/1.two-sum.js
+++ javascript-leetcode/easy/1.two-sum.js
@@ -11,26 +11,33 @@
  * @return {number[]}
  */
 
-const nums = [2,7,11,15]
-const target = 9
-const result = twoSum(nums, target)
+// function twoSum (nums, target) {
+//     const suitMap = new Map();
+
+//     for (let i = 0; i < nums.length; i++) {
+//         const num = nums[i];
+//         const suit = target - num;
+
+//         if (suitMap.has(num)) {
+//             return [suitMap.get(num), i];
+//         }
+
+//         suitMap.set(suit, i)
+//     }
+
+//     return [];
+// };
 
 
-function twoSum (nums, target) {
-    const suitMap = new Map();
-
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        const suit = target - num;
-
-        if (suitMap.has(num)) {
-            return [suitMap.get(num), i];
-        }
-
-        suitMap.set(suit, i)
-    }
-
-    return [];
-};
+function twoSum(nums, target) {
+    
+}
 // @lc code=end
+
+
+
+
+
+
+
 
